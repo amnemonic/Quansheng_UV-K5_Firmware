e79820c5795f599aaf0564df8f7822d3ec004a86
Adam Mnemonic <amnemonic@users.noreply.github.com>
2023-06-09 13:17:32 +0200

    Negative mod

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -7,6 +7,7 @@ qsfirm.py unpack k5_v2.01.26_publish.bin temp\fw.dec.bin temp\fw.ver.bin
 mod_enable_tx_50to850.py temp\fw.dec.bin
 mod_custom_steps.py temp\fw.dec.bin
 mod_custom_freq_ranges.py temp\fw.dec.bin
+rem mod_negative_screen.py temp\fw.dec.bin
 
 
 qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_MODDED.bin
