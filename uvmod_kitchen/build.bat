f38c9c3d1a82f2597b4e76204fdba7381f2bb80f
Adam Mnemonic <amnemonic@users.noreply.github.com>
2023-06-10 18:44:26 +0200

    custom NOAA freqs mod

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -9,6 +9,8 @@ mod_custom_steps.py temp\fw.dec.bin
 mod_custom_freq_ranges.py temp\fw.dec.bin
 rem mod_negative_screen.py temp\fw.dec.bin
 rem mod_change_contrast.py temp\fw.dec.bin
+rem mod_custom_steps.py temp\fw.dec.bin
+rem mod_custom_noaa_freqs.py temp\fw.dec.bin
 
 
 qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_MODDED.bin
