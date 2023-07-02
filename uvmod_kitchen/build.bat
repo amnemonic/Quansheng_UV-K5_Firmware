a32da1e8ac6b7ba071b5be89fef69176a85956ae
Adam Mnemonic <amnemonic@users.noreply.github.com>
2023-07-02 14:44:35 +0200

    Update build.bat

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -1,30 +1,30 @@
 @rmdir /q /s temp 2>NUL
 @mkdir temp
 
-qsfirm.py unpack k5_v2.01.26_publish.bin temp\fw.dec.bin temp\fw.ver.bin
+python qsfirm.py unpack k5_v2.01.26_publish.bin temp\fw.dec.bin temp\fw.ver.bin
 
 :: here comment or uncomment mods
-    mod_battery_icon.py temp\fw.dec.bin
-rem mod_negative_screen.py temp\fw.dec.bin
-rem mod_change_contrast.py temp\fw.dec.bin
-rem mod_mic_gain.py temp\fw.dec.bin
+    python mod_battery_icon.py temp\fw.dec.bin
+rem python mod_negative_screen.py temp\fw.dec.bin
+rem python mod_change_contrast.py temp\fw.dec.bin
+rem python mod_mic_gain.py temp\fw.dec.bin
 
-rem mod_custom_font.py temp\fw.dec.bin
-rem mod_custom_font_DO7OO.py temp\fw.dec.bin
-rem mod_custom_font_VCR.py temp\fw.dec.bin
+rem python mod_custom_font.py temp\fw.dec.bin
+rem python mod_custom_font_DO7OO.py temp\fw.dec.bin
+rem python mod_custom_font_VCR.py temp\fw.dec.bin
 
-    mod_custom_freq_ranges.py temp\fw.dec.bin
-rem mod_custom_noaa_freqs.py temp\fw.dec.bin
+    python mod_custom_freq_ranges.py temp\fw.dec.bin
+rem python mod_custom_noaa_freqs.py temp\fw.dec.bin
 
-rem mod_custom_steps.py temp\fw.dec.bin
-    mod_more_freq_steps.py temp\fw.dec.bin
+rem python mod_custom_steps.py temp\fw.dec.bin
+    python mod_more_freq_steps.py temp\fw.dec.bin
 
-rem mod_disable_tx_completely.py temp\fw.dec.bin
-rem mod_enable_tx_50to850.py temp\fw.dec.bin
-rem mod_enable_tx_50to850_except_airband.py temp\fw.dec.bin
+rem python mod_disable_tx_completely.py temp\fw.dec.bin
+rem python mod_enable_tx_50to850.py temp\fw.dec.bin
+rem python mod_enable_tx_50to850_except_airband.py temp\fw.dec.bin
 
-rem mod_menu_strings.py temp\fw.dec.bin
+rem python mod_menu_strings.py temp\fw.dec.bin
+:: end of mods
 
-
-qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_MODDED.bin
+python qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_MODDED.bin
 
