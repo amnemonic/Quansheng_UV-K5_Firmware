55f5c70e513499c2336ced53060616afcb116aed
Adam Mnemonic <amnemonic@users.noreply.github.com>
2023-07-07 21:34:01 +0200

    Create mod_disable_freq_copy_timeout.py

---
+++ uvmod_kitchen/mod_disable_freq_copy_timeout.py
@@ -0,0 +1,18 @@
+# nothing to configure here
+
+# After applying this mod, the FREQ COPY function ([F]+4) will run indefinetely or if user press [Exit] button
+
+##--------------------- do not modify below this line ---------------------------------------------------
+import os,sys,struct
+print('Running',os.path.basename(sys.argv[0]),'mod...')
+
+fw =  bytearray(open(sys.argv[1],'rb').read())
+
+
+fw[0x4BBC:0x4BBC+2] = b'\x00\xBF'  if fw[0x4BBC:0x4BBC+2] == b'\x52\x1C'  else print('Function not found @0x4BBC. Wrong FW version or already patched')
+
+
+if fw[0x4BBC:0x4BBC+2] == b'\x00\xBF': print('SCAN FREQ function patched successfully')
+
+
+open(sys.argv[1],'wb').write(fw)
