4ca0fdbcec43f2c19952f7f2345f3041239dd681
Andrej <axa1378@cwru.edu>
2023-07-27 10:09:17 +0200

    Add mod_roger_mototrbo_like (#61)
    * Create mod_roger_mototrbo_like.py
    
    * Update build.bat

---
+++ uvmod_kitchen/mod_roger_mototrbo_like.py
@@ -0,0 +1,22 @@
+# If you want, configure these two parameters to change the frequencies of the two tones.
+# The delay, sadly, is fixed to (tone 1 for 150ms), (tone 2 for 80ms) for now because I'm lazy.
+tone1 = int(1540 * 10.32444) # change 1540 to any frequency in Hz
+tone2 = int(1310 * 10.32444) # change 1310 to any frequency in Hz
+
+##--------------------- do not modify below this line ---------------------------------------------------
+import os,sys,struct
+print('Running',os.path.basename(sys.argv[0]),'mod...')
+
+fw =  bytearray(open(sys.argv[1],'rb').read())
+
+if fw[0xaed0:0xaed0+4] == struct.pack('<I',0x142a) and fw[0xaed4:0xaed4+4] == struct.pack('<I',0x1c3b):
+    print('Changing roger beep tones...')
+    fw[0xaed0:0xaed0+4] = struct.pack('<I',0x3e1c)
+    fw[0xaed4:0xaed4+4] = struct.pack('<I',0x34d5)
+    fw[0xae9a] = 0x96
+else:
+    print('ERROR: Cant find function')
+
+
+open(sys.argv[1],'wb').write(fw)
+
