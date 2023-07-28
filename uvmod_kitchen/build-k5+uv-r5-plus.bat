6b3ba4ec27d41575a5453b1316010b5f20fba288
spm81 <55717158+spm81@users.noreply.github.com>
2023-07-28 20:27:15 +0100

    Update build.bat by spm81

---
+++ uvmod_kitchen/build-k5+uv-r5-plus.bat
@@ -0,0 +1,50 @@
+@rmdir /q /s temp 2>NUL
+@mkdir temp
+
+rem python qsfirm.py unpack k5_v2.01.26_publish.bin temp\fw.dec.bin temp\fw.ver.bin
+python qsfirm.py unpack uv_k5_26_rssi_sbar_encoded_v60.bin temp\fw.dec.bin temp\fw.ver.bin
+
+
+:: mods by https://github.com/piotr022
+:: please choose only one of them and always 
+:: place as first mod in this batch file
+rem python mod_piotr022_rssi_sbar.py temp\fw.dec.bin
+rem mod_piotr022_rssi_printer.py temp\fw.dec.bin
+
+
+
+:: here comment or uncomment mods
+rem python mod_battery_icon.py temp\fw.dec.bin
+rem python mod_negative_screen.py temp\fw.dec.bin
+rem python mod_change_contrast.py temp\fw.dec.bin
+rem python mod_mic_gain.py temp\fw.dec.bin
+
+rem python mod_custom_font.py temp\fw.dec.bin
+rem python mod_custom_font_DO7OO.py temp\fw.dec.bin
+rem python mod_custom_font_VCR.py temp\fw.dec.bin
+
+rem python mod_custom_freq_ranges.py temp\fw.dec.bin
+rem python mod_custom_noaa_freqs.py temp\fw.dec.bin
+
+rem python mod_custom_steps.py temp\fw.dec.bin
+rem python mod_more_freq_steps_and_backlight_duration.py temp\fw.dec.bin
+
+rem python mod_disable_tx_completely.py temp\fw.dec.bin
+rem python mod_enable_tx_50to850.py temp\fw.dec.bin
+rem python mod_enable_tx_50to850_except_airband.py temp\fw.dec.bin
+rem python mod_change_tx_limits.py temp\fw.dec.bin
+rem python mod_menu_strings.py temp\fw.dec.bin
+    python mod_custom_bootscreen_narrow_pmr446.py temp\fw.dec.bin
+rem python mod_enable_swd_port.py temp\fw.dec.bin
+python mod_enable_am_everywhere.py temp\fw.dec.bin
+python src\new_0x051f_handler\mod_051f_ramreader.py temp\fw.dec.bin
+python qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_MODDED.bin
+
+python mod_change_version_to_uv-5r_plus.py temp\fw.dec.bin
+python src\new_0x051f_handler\mod_051f_ramreader.py temp\fw.dec.bin
+python qsfirm.py pack temp\fw.dec.bin fw.veruv5rplus.bin uv-r5_v4.00.26_MODDED.bin
+
+
+:: end of mods
+
+
