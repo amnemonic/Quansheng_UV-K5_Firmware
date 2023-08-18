c67599cebcf44183126d9d894f64d60685d5d08b
IK8JHL <francescocasertano@libero.it>
2023-08-18 16:03:15 +0200

    Update build.bat
    Add 1750 tone burst change in batch

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -16,6 +16,7 @@ rem mod_piotr022_rssi_printer.py temp\fw.dec.bin
 
 
 :: here comment or uncomment mods
+    python mod_change_Tone_1750Hz.py temp\fw.dec.bin    
     python mod_battery_icon.py temp\fw.dec.bin
 rem python mod_negative_screen.py temp\fw.dec.bin
 rem python mod_change_contrast.py temp\fw.dec.bin
@@ -26,17 +27,19 @@ rem python mod_custom_font_DO7OO.py temp\fw.dec.bin
 rem python mod_custom_font_VCR.py temp\fw.dec.bin
 
     python mod_custom_freq_ranges.py temp\fw.dec.bin
-rem python mod_custom_noaa_freqs.py temp\fw.dec.bin
+    python mod_change_tx_limits.py temp\fw.dec.bin
+    python mod_custom_noaa_freqs.py temp\fw.dec.bin
 
 rem python mod_custom_steps.py temp\fw.dec.bin
     python mod_more_freq_steps_and_backlight_duration.py temp\fw.dec.bin
-
+rem python mod_double_abr_values.py temp\fw.dec.bin
 rem python mod_disable_tx_completely.py temp\fw.dec.bin
-rem python mod_enable_tx_50to850.py temp\fw.dec.bin
+    python mod_enable_tx_50to850.py temp\fw.dec.bin
 rem python mod_enable_tx_50to850_except_airband.py temp\fw.dec.bin
 
-rem python mod_menu_strings.py temp\fw.dec.bin
+    python mod_menu_strings.py temp\fw.dec.bin
 rem python mod_custom_bootscreen.py temp\fw.dec.bin
+rem python mod_custom_bootscreen_narrow.py temp\fw.dec.bin
 rem python mod_enable_swd_port.py temp\fw.dec.bin
 
 python src\new_0x051f_handler\mod_051f_ramreader.py temp\fw.dec.bin
@@ -45,5 +48,5 @@ python mod_roger_mototrbo_like.py temp\fw.dec.bin
 :: end of mods
 
 @echo Repacking firmware...
-python qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_MODDED.bin
+python qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_IK8JHL.bin
 
