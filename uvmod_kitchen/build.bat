ce7a22d247fd2c1ec0b37598da8a0c56730f8687
IK8JHL <francescocasertano@libero.it>
2023-08-18 17:27:34 +0200

    Add files via upload
    add in batch for increases ABR

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -11,34 +11,37 @@ python qsfirm.py unpack k5_v2.01.26_publish.bin temp\fw.dec.bin temp\fw.ver.bin
 :: please choose only one of them and always 
 :: place as first mod in this batch file
 rem mod_piotr022_rssi_sbar.py temp\fw.dec.bin
-rem mod_piotr022_rssi_printer.py temp\fw.dec.bin
+    mod_piotr022_rssi_printer.py temp\fw.dec.bin
 
 
 
 :: here comment or uncomment mods
-    python mod_change_Tone_1750Hz.py temp\fw.dec.bin    
+
+    python mod_change_Tone_1750Hz.py temp\fw.dec.bin
     python mod_battery_icon.py temp\fw.dec.bin
 rem python mod_negative_screen.py temp\fw.dec.bin
 rem python mod_change_contrast.py temp\fw.dec.bin
 rem python mod_mic_gain.py temp\fw.dec.bin
-
+    
 rem python mod_custom_font.py temp\fw.dec.bin
 rem python mod_custom_font_DO7OO.py temp\fw.dec.bin
 rem python mod_custom_font_VCR.py temp\fw.dec.bin
 
     python mod_custom_freq_ranges.py temp\fw.dec.bin
     python mod_change_tx_limits.py temp\fw.dec.bin
+
     python mod_custom_noaa_freqs.py temp\fw.dec.bin
 
 rem python mod_custom_steps.py temp\fw.dec.bin
     python mod_more_freq_steps_and_backlight_duration.py temp\fw.dec.bin
+    python mod_increases_abr_values.py temp\fw.dec.bin
 rem python mod_double_abr_values.py temp\fw.dec.bin
 rem python mod_disable_tx_completely.py temp\fw.dec.bin
     python mod_enable_tx_50to850.py temp\fw.dec.bin
 rem python mod_enable_tx_50to850_except_airband.py temp\fw.dec.bin
 
     python mod_menu_strings.py temp\fw.dec.bin
-rem python mod_custom_bootscreen.py temp\fw.dec.bin
+    python mod_custom_bootscreen.py temp\fw.dec.bin
 rem python mod_custom_bootscreen_narrow.py temp\fw.dec.bin
 rem python mod_enable_swd_port.py temp\fw.dec.bin
 
