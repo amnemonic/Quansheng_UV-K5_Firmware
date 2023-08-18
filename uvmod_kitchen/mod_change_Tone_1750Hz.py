d874645fbd491395628af3ddd349a878c8146812
Adam Mnemonic <amnemonic@users.noreply.github.com>
2023-08-18 20:23:27 +0200

    Create mod_change_Tone_1750Hz.py
    Co-Authored-By: IK8JHL <20833020+ik8jhl@users.noreply.github.com>

---
+++ uvmod_kitchen/mod_change_Tone_1750Hz.py
@@ -0,0 +1,21 @@
+# If you want, change the frequencies of the tone burst.
+
+tone = int(1750) # change 1750 to any frequency in Hz
+
+
+##--------------------- do not modify below this line ---------------------------------------------------
+import os,sys,struct
+print('Running',os.path.basename(sys.argv[0]),'mod...')
+
+fw =  bytearray(open(sys.argv[1],'rb').read())
+
+if fw[0x29cc] == 0xd6 and fw[0x29cd] == 0x06 :
+    print('Changing tone burst frequency to',tone,'Hz')
+    fw[0x29cc:0x29cc+4] = struct.pack('<I',tone)
+    
+else:
+    print('ERROR: Cant find function')
+
+
+open(sys.argv[1],'wb').write(fw)
+
